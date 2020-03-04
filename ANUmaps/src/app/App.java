7bf9db26eed59e06198ebeae6e5c5692ddfd5f44
Adi <u6051965@anu.edu.au>
2020-03-04 23:37:50 +1100

    changed app.java to strip out useless links

--- ANUmaps/src/app/App.java
+++ ANUmaps/src/app/App.java
@@ -1,7 +1,8 @@
 package app;
+// edit this to strip out useless links from our data
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Test");
     }
 }
